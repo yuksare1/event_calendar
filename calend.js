685c7d25d8305b51ed639f6adae5a968bb83a96c
yuksare1 <yuksare@gmail.com>
2017-04-10 18:58:46 +0300

    initial commit

---
+++ calend.js
@@ -0,0 +1,25 @@
+var app = angular.module("poe_calendar",[]);
+app.controller('poeController',function poeController($scope) {
+  $scope.list = list;
+  $scope.selection = $scope.types();
+  
+  $scope.types = function(){
+   return _.keys(_.groupBy(this.list,function(item){return item.type;}));
+  };
+
+  $scope.toggleSelection = function (typeName) {
+    var idx = $scope.selection.indexOf(typeName);
+ if (idx > -1) {
+      $scope.selection.splice(idx, 1);
+    }
+ else {
+      $scope.selection.push(typeName);
+    }
+  };
+
+  $scope.filterFunction = function(fltr){
+   return function( item ) {
+     return $scope.selection.indexOf(item.type) !== -1;
+   };
+  };
+});
---
+++ list.js
@@ -0,0 +1,5 @@
+var list = [
+{name:"name1",type:"type1",date:'2017-10-28',url:"http://localhost"},
+{name:"name2",type:"type2",date:'2017-10-11',url:""},
+{name:"name3",type:"type3",date:'2017-10-12',url:""}
+]
