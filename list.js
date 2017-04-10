f8438bc22cd6cb93aca02ea1f6871f85b7d9e6b6
yuksare1 <yuksare@gmail.com>
2017-04-11 00:34:35 +0300

    list update and design

--- list.js
+++ list.js
@@ -1,7 +1,48 @@
 ﻿var list = [
-{name:"name1",type:"бревет",date:'2017-10-28',url:"http://localhost"},
-{name:"name2",type:"type2 2",date:'2017-10-11',url:""},
-{name:"name3",type:"type3",date:'2017-10-12',url:""},
-{name:"name3",type:"марафон",date:'2017-10-12',url:""},
-{name:"name3",type:"хцм",date:'2017-10-12',url:""},
+
+{name:"MinskCityRace 2017 Городская приключенческая гонка 30.04.2017",type:"ПГ и ориентирование",date:'2017-04-30',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=111557"},
+
+
+{name:"24-25.06.2017. Приключенческая гонка Active Zone Adventure Race-2017. Кубок ПГ ПромвадТур'17",type:"ПГ и ориентирование",date:'2017-06-24',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=113307"},
+
+{name:"Налибоки 2017. Любительский марафон. 20-21 мая 2017. Обсуждение, новости, пожелания",type:"ПГ и ориентирование",date:'2017-05-20',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=114579"},
+
+{name:"RACE ONE - 08.04.2017 ",type:"ПГ и ориентирование",date:'2017-04-08',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=111899"},
+
+
+{name:"Rogaining Cup 2017 - 17/18 июня ",type:"ПГ и ориентирование",date:'2017-06-17',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=113279"},
+
+{name:"Марафон Поліська Січ 2017 (Туристичний) 3 - 4 - 5 червня 2017 ",type:"ПГ и ориентирование",date:'2017-06-03',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=112493"},
+
+{name:"14-15 января 2017 гонка Зима минус 100 ",type:"ПГ и ориентирование",date:'2017-01-14',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=109920"},
+
+
+
+
+
+
+{name:"15 апреля - Фан-ХСО на Трассе-62 и не только...",type:"Гонки КК и XCM",date:'2017-04-15',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=114486"},
+
+{name:"КК гонка, Лида, Зорька",type:"Гонки КК и XCM",date:'2017-05-07',url:""},
+
+{name:"Beskidy MTB Trophy 15-18.06.2017",type:"Гонки КК и XCM",date:'2017-06-15',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=108809"},
+
+
+
+
+
+{name:"15.04.2017, Бревет 200+км (Б200), Вилейская петля 2",type:"Бреветы, шоссе, дороги",date:'2017-04-15',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=112805"},
+
+{name:"Бревет 300км.АСР. 10.06.2017 Рівне. Україна.  ",type:"Бреветы, шоссе, дороги",date:'2017-06-10',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=112405"},
+
+
+
+
+
+
+
+{name:" Жук-Трейл. Дружеский пробег. 08.04.2017",type:"Прочие события",date:'2017-04-08',url:"http://forum.poehali.net/index.php?board=21;action=display;threadid=112598"},
+
+{name:" Велокарнавал VIVA Ровар!-2017",type:"Прочие события",date:'2017-05-13',url:""},
+
 ]
