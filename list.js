5a21e52269c941f65465138cc639c56cee3aa224
yuksare1 <yuksare@gmail.com>
2017-04-10 20:03:32 +0300

    events type colors

--- list.js
+++ list.js
@@ -1,5 +1,7 @@
-var list = [
-{name:"name1",type:"type1",date:'2017-10-28',url:"http://localhost"},
-{name:"name2",type:"type2",date:'2017-10-11',url:""},
-{name:"name3",type:"type3",date:'2017-10-12',url:""}
+﻿var list = [
+{name:"name1",type:"бревет",date:'2017-10-28',url:"http://localhost"},
+{name:"name2",type:"type2 2",date:'2017-10-11',url:""},
+{name:"name3",type:"type3",date:'2017-10-12',url:""},
+{name:"name3",type:"марафон",date:'2017-10-12',url:""},
+{name:"name3",type:"хцм",date:'2017-10-12',url:""},
 ]
