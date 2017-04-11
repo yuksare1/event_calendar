73d547d28af657806c2c952ceb60f82ffdd13f79
Roman <Roman@example.com>
2017-04-11 15:50:27 +0100

    by month group

--- calend.js
+++ calend.js
@@ -1,26 +1,22 @@
 var app = angular.module("poe_calendar",[]);
 app.controller('poeController',function poeController($scope) {
-  $scope.list = list;
+	$scope.list = list;
 
-  $scope.types = function(){
-   return _.keys(_.groupBy(this.list,function(item){return item.type;}));
-  };
+	$scope.types = function(){
+	return _.keys(_.groupBy(this.list,function(item){return item.type;}));
+	};
 
-  $scope.selection = $scope.types();
+	$scope.selection = $scope.types();
 
-  $scope.toggleSelection = function (typeName) {
-var idx = $scope.selection.indexOf(typeName);
- if (idx > -1) {
-      $scope.selection.splice(idx, 1);
-    }
- else {
-      $scope.selection.push(typeName);
-    }
-  };
+	$scope.toggleSelection = function (typeName) {
+		var idx = $scope.selection.indexOf(typeName);
+		if (idx > -1)
+			$scope.selection.splice(idx, 1);
+		else
+			$scope.selection.push(typeName);
+	};
 
-  $scope.filterFunction = function(fltr){
-   return function( item ) {
-     return $scope.selection.indexOf(item.type) !== -1;
-   };
-  };
+	$scope.filterFunction = function(item){
+		return $scope.selection.indexOf(item.type) !== -1;
+	};
 });
